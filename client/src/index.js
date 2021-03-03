fa55fc7e2cae862a5285f1c563213d5afc516567
Mack Ramirez <mack.n.ramirez@gmail.com>
2021-03-03 09:45:32 -0800

    initial heroku deploy fe

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 import axios from "axios";
 import history from './history';
 
-axios.defaults.baseURL="http://localhost:7900/";
+axios.defaults.baseURL="https://jqr-collection-portfolio-api.herokuapp.com";
 axios.defaults.headers.common[ 'Authorization'] = 'Bearer' + localStorage.getItem('token');
 ReactDOM.render(
   <React.StrictMode>
