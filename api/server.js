2444a479914bf48ed50466de11a44041c4d4b9c8
Mack Ramirez <mack.n.ramirez@gmail.com>
2021-03-03 05:34:27 -0800

    Preparing server for Heroku

--- api/server.js
+++ api/server.js
@@ -35,6 +35,16 @@ app.use('/username', userRouter);
 app.use('/portfolio-items', portfolioRouter);
 app.use('/car-collection', carRouter);
 
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+
+
 app.listen(port, () => {
     console.log(`Server is listening on port: ${port}`)
 })
